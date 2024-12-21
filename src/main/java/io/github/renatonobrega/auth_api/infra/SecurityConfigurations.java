fcb48287e7240543e4c0fff972e0017af8d22af5
Renato28 <renatonobrega87@gmail.com>
2024-12-20 21:41:45 -0300

    implementando a classe SecurityFilter e SecurityConfigurations e atualizando pom.xml

---
+++ src/main/java/io/github/renatonobrega/auth_api/infra/SecurityConfigurations.java
@@ -0,0 +1,10 @@
+package io.github.renatonobrega.auth_api.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+}
